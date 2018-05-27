0ed1046047dbf1e59a0db4cfe5cdba6b4be31535
Chairat Onyaem <pacroy@gmail.com>
2018-05-27 15:16:44 -0500

    Changed to GetMapping

--- src/main/java/me/chairat/spring/boot/workshop/helloworld/HelloWorldController.java
+++ src/main/java/me/chairat/spring/boot/workshop/helloworld/HelloWorldController.java
@@ -1,13 +1,12 @@
 package me.chairat.spring.boot.workshop.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+    @GetMapping(path = "/helloworld")
     public String helloWorld() {
         return "Hello World";
     }
