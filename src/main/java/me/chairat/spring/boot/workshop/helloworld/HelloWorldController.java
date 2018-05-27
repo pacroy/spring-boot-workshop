eff5bf49eccf9dd0dc899662fdd169dc26bed08f
Chairat Onyaem <pacroy@gmail.com>
2018-05-27 16:15:47 -0500

    Added Path variable

--- src/main/java/me/chairat/spring/boot/workshop/helloworld/HelloWorldController.java
+++ src/main/java/me/chairat/spring/boot/workshop/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package me.chairat.spring.boot.workshop.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,11 @@ public class HelloWorldController {
     @GetMapping(path = "/helloworld-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/helloworld-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s",name));
     }
 
 }
