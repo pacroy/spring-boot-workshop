f2ee2df45224574060e59706b7623cc7a99652e5
Chairat Onyaem <pacroy@gmail.com>
2018-05-27 21:08:13 -0500

    Added UserResource retrieveAllUsers

---
+++ src/main/java/me/chairat/spring/boot/workshop/user/UserResource.java
@@ -0,0 +1,20 @@
+package me.chairat.spring.boot.workshop.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+}
